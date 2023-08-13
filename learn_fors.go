8c2c315a62291a5daa09cd869939475623e0c2bd
Yurcik45 <yurcik45mail@gmail.com>
2023-08-13 17:26:52 +0300

    a bit more about objects

--- learn_fors.go
+++ learn_fors.go
@@ -13,10 +13,35 @@ func print_slice_arr(arr []int) {
   }
 }
 
+func print_obj_pairs(obj map[string]interface{}) {
+  for k, v := range obj {
+    fmt.Println("key: ", k, "value: ", v)
+  }
+}
+
+type User struct {
+  name string
+  age int
+}
+
 func main() {
   some_arr := [4]int{1,2,3,4}
   print_elements(some_arr)
 
   some_slice_arr := []int{1,2,3}
   print_slice_arr(some_slice_arr)
+
+  some_obj := map[string]interface{}{
+    "name": "Rusik",
+    "age": 90,
+  }
+  print_obj_pairs(some_obj)
+
+  for _, c := range "abcd" {
+    fmt.Print(" ", c)
+  }
+  fmt.Println()
+
+  user := User{name: "yurcik", age: 20}
+  fmt.Println("user str: ", user.name)
 }
